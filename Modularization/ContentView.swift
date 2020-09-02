fa4dcfdcf08eb02a8680d2006caaabfb5f307700
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2020-09-02 18:25:25 +0700

    Initial Commit

---
+++ Modularization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modularization
+//
+//  Created by Mobile Dev on 02/09/20.
+//  Copyright © 2020 Amarbank. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Modularization")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
